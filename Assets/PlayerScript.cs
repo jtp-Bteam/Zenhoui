686dda689716b5427431465b0b3d55fbec803919
gasagasa <sirane1000@gmail.com>
2017-07-18 19:07:10 +0900

    a

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+    Vector3 pos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+		pos = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 pos = transform.position;
-        pos.x += CrossPlatformInputManager.GetAxisRaw("Horizontal") * 0.5f;
-        pos.z += CrossPlatformInputManager.GetAxisRaw("Vertical") * 0.5f;
+        pos.x += CrossPlatformInputManager.GetAxisRaw("HorizontalLeft") * 0.3f;
+        pos.z += CrossPlatformInputManager.GetAxisRaw("VerticalLeft") * 0.3f;
         transform.position = pos;
-	}
+        double radian = System.Math.Atan2(CrossPlatformInputManager.GetAxis("VerticalRight"), CrossPlatformInputManager.GetAxis("HorizontalRight"));
+        int dosu = (int)((180 * radian) / System.Math.PI) - 90;
+        transform.rotation = Quaternion.Euler(0, -dosu, 0);
+    }
 }
