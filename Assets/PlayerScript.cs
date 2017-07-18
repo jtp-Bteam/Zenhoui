61f170a23b6c0c9dd18a5e59a3351fed9d5bf173
gasagasa <sirane1000@gmail.com>
2017-07-18 16:58:19 +0900

    a

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += CrossPlatformInputManager.GetAxisRaw("Horizontal") * 0.5f;
+        pos.z += CrossPlatformInputManager.GetAxisRaw("Vertical") * 0.5f;
+        transform.position = pos;
+	}
+}
