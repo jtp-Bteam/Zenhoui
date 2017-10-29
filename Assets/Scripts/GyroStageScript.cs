101da2fbc93150d4d2732143e6a36ac2aa81a429
Yasuda Kousuke <14ec109@ms.dendai.ac.jp>
2017-10-29 13:40:57 +0900

    masterを上書きした所はDescriptionに書いたよ
    1. タグの追加 : CrushZone
    2. EnemyBulletのレイヤーを2に変更
    他は全部新規追加

---
+++ Assets/Scripts/GyroStageScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroStageScript : AbstractStageScript {
+
+    GameObject bossObj;
+
+	// Use this for initialization
+	void Start () {
+
+        playerObj = (GameObject)Resources.Load("Prefabs/Player");
+        bossObj = (GameObject)Resources.Load("Prefabs/SearchAndDestroy");
+
+        GameObject player = Instantiate(playerObj, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        player.name = "Player";
+
+        //GenerateEnemy();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void GenerateEnemy()
+    {
+        GameObject enemy = Instantiate(enemyObj, new Vector3(-20 + Random.value * 40, 0, -20 + Random.value * 40), Quaternion.identity) as GameObject;
+        enemy.name = "Enemy";
+    }
+
+    public void GenerateBoss()
+    {
+        Destroy(GameObject.Find("GyroObstacle"));
+        GameObject boss = Instantiate(bossObj, new Vector3(-14, 0, 14), Quaternion.identity);
+        boss.name = "Enemy";
+    }
+}
